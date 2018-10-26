e9970d9cd23461d3312c3bcf0726f9dd297b6415
Philip Doxakis <doxakisp@gmail.com>
2018-10-25 23:58:56 -0400

    Init

---
+++ CountLinesExtension/CountLinesExtension.cs
@@ -0,0 +1,64 @@
+﻿using SharpShell.Attributes;
+using SharpShell.SharpContextMenu;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CountLinesExtension
+{
+	[ComVisible(true)]
+	[COMServerAssociation(AssociationType.ClassOfExtension, ".txt")]
+	public class CountLinesExtension : SharpContextMenu
+	{
+		protected override bool CanShowMenu()
+		{
+			//  We will always show the menu.
+			return true;
+		}
+		
+		protected override ContextMenuStrip CreateMenu()
+		{
+			//  Create the menu strip.
+			var menu = new ContextMenuStrip();
+
+			//  Create a 'count lines' item.
+			var itemCountLines = new ToolStripMenuItem
+			{
+				Text = "Count Lines",
+				Image = Resource.icon
+			};
+
+			//  When we click, we'll call the 'CountLines' function.
+			itemCountLines.Click += (sender, args) => CountLines();
+
+			//  Add the item to the context menu.
+			menu.Items.Add(itemCountLines);
+
+			//  Return the menu.
+			return menu;
+		}
+
+		private void CountLines()
+		{
+			//  Builder for the output.
+			var builder = new StringBuilder();
+
+			//  Go through each file.
+			foreach (var filePath in SelectedItemPaths)
+			{
+				//  Count the lines.
+				builder.AppendLine(string.Format("{0} - {1} Lines",
+				  Path.GetFileName(filePath), File.ReadAllLines(filePath).Length));
+			}
+
+			//  Show the ouput.
+			MessageBox.Show(builder.ToString());
+		}
+	}
+}
--- CountLinesExtension/Properties/AssemblyInfo.cs
+++ CountLinesExtension/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ShellExtensionDemo")]
+[assembly: AssemblyTitle("CountLinesExtension")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ShellExtensionDemo")]
+[assembly: AssemblyProduct("CountLinesExtension")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4b3beff1-9fc3-4d03-ad73-c2ead85a4358")]
+[assembly: Guid("729fc918-f5c0-483c-b5c7-4a4230b493b4")]
 
 // Version information for an assembly consists of the following four values:
 //
